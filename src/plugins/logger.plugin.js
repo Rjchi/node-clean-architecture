0e94da75f72a0746720a5db769d0146b0a371aa5
Rjchi <richardospina18@gmail.com>
2025-03-15 20:15:41 -0500

    [Feat] Agregar la configuración del logger (winston)

---
+++ src/plugins/logger.plugin.js
@@ -0,0 +1,28 @@
+/**
+ * * Este logger lo utilizamos para hacer seguimiento a todos los
+ * * logs que se puedan generar en mi aplicación, es muy util
+ * * particularmente en producción
+ */
+
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    /**
+     * ! nos genera un archivo con todos los logs, llamado error.log
+     * ! y un archivo llamado combined.log con la configuración del logger
+     */
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+module.exports = function buildLogger(service) {
+  return {
+    log: (msg) => {
+      logger.log("info", { msg, service });
+    },
+  };
+};
